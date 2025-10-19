7d4d7514b7a0a766d9f5ea18cca0fda901a11940
Abdallah <abdallahmohamedama12@gmail.com>
2025-10-19 18:53:24 +0300

    Refactor URL routing to use function-based views for task management

--- task_management/tasks/urls.py
+++ task_management/tasks/urls.py
@@ -1,7 +1,9 @@
-from rest_framework.routers import DefaultRouter
-from .views import TaskViewSet
+from django.urls import path
+from . import views
 
-router = DefaultRouter()
-router.register(r'tasks', TaskViewSet, basename='task')
-
-urlpatterns = router.urls
+urlpatterns = [
+    path('tasks/', views.TaskListCreateView.as_view(), name='task-list-create'),
+    path('tasks/<int:pk>/', views.TaskRetrieveUpdateDeleteView.as_view(), name='task-detail'),
+    path('tasks/<int:pk>/mark_complete/', views.MarkTaskCompleteView.as_view(), name='task-complete'),
+    path('tasks/<int:pk>/mark_incomplete/', views.MarkTaskIncompleteView.as_view(), name='task-incomplete'),
+]
