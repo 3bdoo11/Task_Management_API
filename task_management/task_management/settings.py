f1005bea52693a91bd1b661626a3f92823caa068
Abdallah <abdallahmohamedama12@gmail.com>
2025-10-19 21:17:53 +0300

    Reorder INSTALLED_APPS for consistency and clarity

--- task_management/task_management/settings.py
+++ task_management/task_management/settings.py
@@ -22,9 +22,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tasks',
     'rest_framework',
-    'rest_framework.authtoken',  
+    'rest_framework.authtoken',
+    'tasks',
+      
 ]
 
 MIDDLEWARE = [
